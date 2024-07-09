433a96478997b57b9e76c8a8402b1479c8f68dcf
David <momachilles@gmail.com>
2024-07-09 18:56:30 +0200

    Create repository model.

---
+++ GithubUsers/Model/Repository.swift
@@ -0,0 +1,13 @@
+//
+//  Repository.swift
+//  GithubUsers
+//
+//  Created by David Alarcon on 9/7/24.
+//
+
+import Foundation
+
+struct Repository: Identifiable {
+  var id: Int
+  var name: String
+}
