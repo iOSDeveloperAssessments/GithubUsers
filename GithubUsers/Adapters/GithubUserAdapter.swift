da005c0ac3e11e43f7bd2099cb9e1b288ca75da0
David <momachilles@gmail.com>
2024-07-09 15:34:05 +0200

    Fix response model.

--- GithubUsers/Adapters/GithubUserAdapter.swift
+++ GithubUsers/Adapters/GithubUserAdapter.swift
@@ -21,6 +21,6 @@ class GithubUserAdapter: Adapter {
   }
 
   func adapt() -> GithubUser {
-    .init(username: adaptee.login, avatarURL: URL(string: adaptee.avatar_url))
+    .init(id:adaptee.id, username: adaptee.login, avatarURL: URL(string: adaptee.avatar_url))
   }
 }
