0276e5eceb78a9317547cb2b14cf604b78349bd5
David <momachilles@gmail.com>
2024-07-09 09:18:37 +0200

    Add endpoint protocol.

---
+++ GithubUsers/Network/Service/Endpoint.swift
@@ -0,0 +1,27 @@
+//
+//  Endpoint.swift
+//  GithubUsers
+//
+//  Created by David Alarcon on 9/7/24.
+//
+
+import Foundation
+
+enum Method: String {
+  case get = "GET"
+
+  var string: String { rawValue }
+}
+
+protocol Endpoint {
+  var scheme: String { get }
+  var host: String { get }
+  var baseURLString: String { get }
+  var path: String { get }
+  var method: Method { get }
+  var queryParameters: [String: String]? { get }
+}
+
+protocol URLRequestable {
+  var urlRequest: URLRequest? { get }
+}
